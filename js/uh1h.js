5453b8621cd875cc1d5ba81a0bd872b9997426f1
Claudio Peralta <cps@MacBook-Pro-de-Claudio.local>
2015-10-07 12:13:09 -0300

    prueba carga imagen top low y high quality

--- js/uh1h.js
+++ js/uh1h.js
@@ -4,7 +4,23 @@
  * and open the template in the editor.
  */
 
+/**
+ * pre load top image high resolution
+ */
+var topImage = $('#myCarousel').css('background-image').replace(/^url\(([^\)]+)\)$/, '$1').replace('_low.jpg', '_high.jpg')
+var image = $('<img/>').css('display', 'non_e');
+$('body').append(image);
+image.on('load', function () {
+	$('#myCarousel').addClass('high');
+	image.remove();
+});
+image.attr('src', topImage);
 
+
+
+/**
+ * navigator menu
+ */
 $("#navbar ul li a[href^='#']").on('click', function (e) {
 
 	// prevent default anchor click behavior
